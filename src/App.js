c44b90b1feae22cd4440208dba57b818acade26c
t.aysin <tymur97@gmail.com>
2020-08-15 21:45:40 +0400

    Add

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Container } from 'react-bootstrap';
+import useFetchJobs from './useFetchJobs';
+
+function App() {
+
+const {jobs, loading, error} = useFetchJobs();
+
+  return (
+   <Container>
+     {loading && <h1>Loading...</h1>}
+     {error && <h1>Error</h1>}
+     <h1>{jobs.length}</h1>
+   </Container>
+  );
+}
+
+export default App;
