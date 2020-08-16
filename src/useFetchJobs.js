97f88948cec6537175fc3ec9903639cf327cbb9e
razbudiv420 <tymur97@gmail.com>
2020-08-16 18:23:32 +0300

    add another one axios request (pagination)

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -4,12 +4,13 @@ import  axios  from "axios";
 const ACTIONS = {
     MAKE_REQUEST: 'make-request',
     GET_DATA: 'get-data',
-    ERROR: 'error'
+    ERROR: 'error',
+    HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
 const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?';
 
-function reducer(state, action) {
+function  reducer(state, action) {
 switch(action.type) {
     case ACTIONS.MAKE_REQUEST:
         return {loading: true, jobs:[]}
@@ -20,29 +21,44 @@ switch(action.type) {
     case ACTIONS.ERROR:
         return {...state, loading: false, error: action.payload.error, jobs: []}
 
+    case ACTIONS.HAS_NEXT_PAGE:
+        return {...state, hasNextPage: action.payload.hasNextPage}
     default: return state
     
 }
 }
 
-export default function useFetchJobs (params, page) {
+const useFetchJobs = (params, page) => {
     const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true})
 useEffect(()=>{
+    const cancelToken1 = axios.CancelToken.source();
     dispatch({type: ACTIONS.MAKE_REQUEST});
     axios.get(BASE_URL, {
+        cancelToken: cancelToken1.token,
         params: {markdown: true, page: page, ...params}
     }).then(res => {
         dispatch({type: ACTIONS.GET_DATA, payload: {jobs: res.data}})
     }).catch(e => {
+        if (axios.isCancel(e)) return 
         dispatch({type: ACTIONS.ERROR, payload: {error: e}})
-    });
+    })
+
+    const cancelToken2 = axios.CancelToken.source();
+    axios.get(BASE_URL, {
+        cancelToken: cancelToken2.token,
+        params: {markdown: true, page: page + 1, ...params}
+    }).then(res => {
+        dispatch({type: ACTIONS.HAS_NEXT_PAGE, payload: {hasNextPage: res.data !== 0}})
+    }).catch(e => {
+        if (axios.isCancel(e)) return 
+        dispatch({type: ACTIONS.ERROR, payload: {error: e}})
+    })
+        return () => {
+            cancelToken1.cancel()
+            cancelToken2.cancel()
+        }
 }, [params, page])
-            return (
-                {
-                    jobs: [],
-                    loading: true,
-                    error: false
-                }
-            );
+            return state
         }
     
+export default useFetchJobs;
