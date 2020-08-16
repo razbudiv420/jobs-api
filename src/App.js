3829d10140af5e81f1e59a32a77f999405a7982f
razbudiv420 <tymur97@gmail.com>
2020-08-16 18:21:08 +0300

    check pagination.jsx and job.jsx at the same date

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Container } from 'react-bootstrap';
 import useFetchJobs from './useFetchJobs';
+import Job from "./job";
+import Paginator from './pagination';
+
 
 function App() {
-
-const {jobs, loading, error} = useFetchJobs();
+const [params, setParams] = useState({});
+const [page, setPage] = useState(1);
+const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page);
 
   return (
-   <Container>
+    
+   <Container className="my-4">
+     <Paginator page={page} setPage={setPage} hasNextPage={hasNextPage}/>
      {loading && <h1>Loading...</h1>}
      {error && <h1>Error</h1>}
-     <h1>{jobs.length}</h1>
+     {jobs.map(job => {
+       return <Job key={job.id} job={job} />
+     })}
+     <Paginator page={page} setPage={setPage} hasNextPage={hasNextPage}/>
    </Container>
+   
   );
 }
 
