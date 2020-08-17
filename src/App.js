44c52c9ea5d565979051177b5a94418b1575fc02
t.aysin <tymur97@gmail.com>
2020-08-17 17:44:00 +0400

    final version

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import { Container } from 'react-bootstrap';
 import useFetchJobs from './useFetchJobs';
 import Job from "./job";
 import Paginator from './pagination';
-
+import SearchForm from './searchForm';
 
 function App() {
 const [params, setParams] = useState({});
 const [page, setPage] = useState(1);
 const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page);
-
+function handleParamChange (e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1)
+    setParams(prevParams => {
+      return {...prevParams, [param]: value}
+    })
+}
   return (
     
    <Container className="my-4">
+     <SearchForm params={params} onParamChange={handleParamChange}></SearchForm>
      <Paginator page={page} setPage={setPage} hasNextPage={hasNextPage}/>
      {loading && <h1>Loading...</h1>}
      {error && <h1>Error</h1>}
